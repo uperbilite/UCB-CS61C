0a681b86427432a729fe62f90441c9ece176c392
uperbilite <uperbilite@gmail.com>
2022-10-11 23:16:56 +0800

    complete lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+	node *tortoise = head;
+	node *hare = head;
+	if (head == NULL) {
+		return 0;
+	}
+	while (hare -> next != NULL && hare -> next -> next != NULL) {
+		hare = hare -> next -> next;
+		tortoise = tortoise -> next;
+		if (tortoise == hare) {
+			return 1;
+		}
+	}
     return 0;
 }
