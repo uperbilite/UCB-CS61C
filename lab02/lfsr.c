62f11ff51b612fe5eb3812b8bbf2944cf6863655
uperbilite <uperbilite@gmail.com>
2022-10-12 15:55:11 +0800

    complete lab2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,12 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+	uint16_t reg0 = (*reg >> 0) & 0x1;
+	uint16_t reg2 = (*reg >> 2) & 0x1;
+	uint16_t reg3 = (*reg >> 3) & 0x1;
+	uint16_t reg5 = (*reg >> 5) & 0x1;
+	uint16_t t = (reg5 ^ (reg3 ^ (reg0 ^ reg2)));
+	*reg >>= 1;
+	*reg = (*reg & ~(1 << 15)) | (t << 15);
 }
 
