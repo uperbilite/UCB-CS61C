9c4384db433084d23aecdfb2f9fff86ddec3b386
uperbilite <uperbilite@gmail.com>
2022-10-17 15:09:57 +0800

    complete lab3 exercise 3 by recursion

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -20,6 +20,7 @@ main:
     addi a0, x0, 10
     ecall # Exit
 
+// iteration
 factorial:
     mv t0, a0
     li t1, 1
@@ -31,4 +32,26 @@ loop:
     j loop
 
 factorial_exit:
+    jr ra
+
+// recursion
+factorial:
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw a0, 0(sp)
+    addi t0, a0, -1
+    bge t0, zero, L1
+    li a0, 1
+    addi sp, sp, 8
+    jr ra
+
+L1:
+    addi a0, a0, -1
+    jal ra, factorial
+
+    mv t1, a0
+    lw ra, 4(sp)
+    lw a0, 0(sp)
+    addi sp, sp, 8
+    mul a0, a0, t1
     jr ra
